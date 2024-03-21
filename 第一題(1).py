6b1945e5b70ab9e49178bd4ef99ed32bcd9d7d19
S12240017 <161661096+chanyixue@users.noreply.github.com>
2024-03-21 08:50:13 +0800

    Add files via upload

---
+++ 第一題(1).py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+my_tuple = ("apple", 25, [1, 2, 3], 3.14)
+
+
+print("第二個元素為", str(my_tuple[1]).upper(), "最後一個元素為", my_tuple[-1])
